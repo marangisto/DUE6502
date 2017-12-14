b0b7d57d0bc554362dcb4eff97c58e7641892b33
Bengt Marten Agren <maraingist@me.com>
2017-12-14 23:48:54 +0800

    First attempt to talk to the 6502 chip.

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,89 @@
-#include <due.h>
+#include "board.h"
+#include <delay.h>
+#include <main.h>
 #include <UART.h>
+#include <stdlib.h>
 
 typedef uart_t uart;
-typedef output_t<D13> BOARD;
-typedef output_t<A0> LED0;
-typedef output_t<A1> LED1;
-typedef output_t<A2> LED2;
-typedef output_t<A3> LED3;
 
 void setup()
 {
     uart::setup<115200>();
-    BOARD::setup();
+
     LED0::setup();
     LED1::setup();
     LED2::setup();
-    LED3::setup();
+
+    // configure pins
+    D0::setup();
+    D1::setup();
+    D2::setup();
+    D3::setup();
+    D4::setup();
+    D5::setup();
+    D6::setup();
+    D7::setup();
+    A0::setup();
+    A1::setup();
+    A2::setup();
+    A3::setup();
+    A4::setup();
+    A5::setup();
+    A6::setup();
+    A7::setup();
+    A8::setup();
+    A9::setup();
+    A10::setup();
+    A11::setup();
+    A12::setup();
+    A13::setup();
+    A14::setup();
+    A15::setup();
+    BE::setup();
+    MLB::setup();
+    RWB::setup();
+    RDY::setup<pull_up>();    // using pull-up as this pin is potenially an output on 6502
+    SYNC::setup();
+    SOB::setup();
+    PHI2::setup();
+    PHI1O::setup();
+    PHI2O::setup();
+    RESB::setup();
+    IRQB::setup();
+    NMIB::setup();
+    VBP::setup();
+
+    // initial values
+
+    SOB::set();             // set overflow
+    IRQB::set();            // interrupt request
+    NMIB::set();            // non-maskable interrupt
+    RESB::set();            // don't hold reset
+    PHI2::clear();          // start with low clock
+    BE::set();              // bus enable
 }
+
+template<class T7, class T6, class T5, class T4, class T3, class T2, class T1, class T0>
+struct byte_t
+{
+    static uint8_t read()
+    {
+        return (static_cast<uint8_t>(T7::get()) << 7)
+             | (static_cast<uint8_t>(T6::get()) << 6)
+             | (static_cast<uint8_t>(T5::get()) << 5)
+             | (static_cast<uint8_t>(T4::get()) << 4)
+             | (static_cast<uint8_t>(T3::get()) << 3)
+             | (static_cast<uint8_t>(T2::get()) << 2)
+             | (static_cast<uint8_t>(T1::get()) << 1)
+             | (static_cast<uint8_t>(T0::get()) << 0)
+             ;
+    }
+};
+
+typedef byte_t<D7, D6, D5, D4, D3, D2, D1, D0> data;
+typedef byte_t<A7, A6, A5, A4, A3, A2, A1, A0> lo_addr;
+typedef byte_t<A15, A14, A13, A12, A11, A10, A9, A8> hi_addr;
+typedef byte_t<MLB, MLB, MLB, RWB, SYNC, PHI1O, PHI2O, VBP> status;
 
 struct toggler_t
 {
@@ -33,30 +100,69 @@ struct toggler_t
     bool m_b;
 };
 
+static inline void print(const char *s)
+{
+    for (const char *p = s; *p; ++p)
+        uart::put_char(*p);
+}
+
+static inline void println(const char *s = "")
+{
+    print(s);
+    uart::put_char('\n');
+}
+
+static inline void print(int i, int base = 10)
+{
+    char buf[32];
+
+    itoa(i, buf, base);
+    print(buf);
+}
+
+static void print_state(int i)
+{
+    print(i);
+    print("  0x");
+    print(hi_addr::read(), 16);
+    print(lo_addr::read(), 16);
+    print(" ");
+    print(data::read(), 16);
+    print(" ");
+    print(0x3f & status::read(), 2);
+    println();
+}
+
 void loop()
 {
     static int i = 0;
-    static toggler_t board(1);
     static toggler_t output0(3);
     static toggler_t output1(5);
     static toggler_t output2(7);
-    static toggler_t output3(11);
 
-    BOARD::set(board.toggle(i));
+    if (i == 0)     // make sure we have a fresh reset
+    {
+        RESB::clear();
+        delay(1);
+        RESB::set();
+    }
+
     LED0::set(output0.toggle(i));
     LED1::set(output1.toggle(i));
     LED2::set(output2.toggle(i));
-    LED3::set(output3.toggle(i));
 
-    delay(50);
-    ++i;
     int c;
+    bool step = false;
  
     while ((c = uart::get_char()) > 0)
+        step = true;
+
+    if (step)
     {
-        uart::put_char('>');
-        uart::put_char(c);
-        uart::put_char('\n');
+        delay(100);
+        print_state(i);
+        PHI2::toggle();
+        ++i;
     }
 }
 
---
+++ board.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <pins.h>
+
+////
+//
+//      6502DUE board pins mapped on SAM3X8E
+//
+////
+
+typedef input_t<pin_t<PC, 5> > D0;
+typedef input_t<pin_t<PC, 4> > D1;
+typedef input_t<pin_t<PC, 3> > D2;
+typedef input_t<pin_t<PC, 2> > D3;
+typedef input_t<pin_t<PC, 1> > D4;
+typedef input_t<pin_t<PD, 10> > D5;
+typedef input_t<pin_t<PA, 7> > D6;
+typedef input_t<pin_t<PD, 9> > D7;
+
+typedef input_t<pin_t<PA, 16> > A0;
+typedef input_t<pin_t<PA, 24> > A1;
+typedef input_t<pin_t<PA, 23> > A2;
+typedef input_t<pin_t<PA, 22> > A3;
+typedef input_t<pin_t<PA, 6> > A4;
+typedef input_t<pin_t<PA, 4> > A5;
+typedef input_t<pin_t<PA, 3> > A6;
+typedef input_t<pin_t<PA, 2> > A7;
+typedef input_t<pin_t<PB, 17> > A8;
+typedef input_t<pin_t<PB, 18> > A9;
+typedef input_t<pin_t<PB, 19> > A10;
+typedef input_t<pin_t<PB, 20> > A11;
+typedef input_t<pin_t<PB, 15> > A12;
+typedef input_t<pin_t<PB, 16> > A13;
+typedef input_t<pin_t<PA, 1> > A14;
+typedef input_t<pin_t<PA, 0> > A15;
+
+typedef output_t<pin_t<PB, 26> > BE;
+typedef input_t<pin_t<PA, 14> > MLB;
+typedef input_t<pin_t<PA, 15> > RWB;
+typedef input_t<pin_t<PD, 0> > RDY;    // use pullup
+typedef input_t<pin_t<PD, 1> > SYNC;
+typedef output_t<pin_t<PD, 2> > SOB;
+typedef output_t<pin_t<PC, 29> > PHI2;
+typedef input_t<pin_t<PD, 7> > PHI1O;
+typedef input_t<pin_t<PD, 8> > PHI2O;
+typedef output_t<pin_t<PC, 14> > RESB;
+typedef output_t<pin_t<PC, 15> > IRQB;
+typedef output_t<pin_t<PC, 16> > NMIB;
+typedef input_t<pin_t<PC, 17> > VBP;
+
+typedef output_t<pin_t<PB, 27> > LED0;
+typedef output_t<pin_t<PA, 21> > LED1;
+typedef output_t<pin_t<PC, 30> > LED2;
+
